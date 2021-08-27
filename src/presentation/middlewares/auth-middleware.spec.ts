55f3f665a40d39e10f0da5605dcdd3ea9863dc9c
Pablo <pablorsantos15@gmail.com>
2021-08-27 18:13:05 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,13 +1,59 @@
 import { forbidden } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors/'
 import { AuthMiddleware } from './auth-middleware'
+import { LoadAccountByToken } from '../../domain/usecases/'
+import { AccountModel } from '../../domain/models/account'
+
+interface SutTypes {
+  sut: AuthMiddleware
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+const makeFakeAccountModel = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email',
+  password: 'any_password'
+})
+
+const makeLoadAccountByTokenStub = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (accessToken: string, role?: string): Promise<AccountModel> {
+      return makeFakeAccountModel()
+    }
+  }
+
+  return new LoadAccountByTokenStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByTokenStub = makeLoadAccountByTokenStub()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+
+  return { sut, loadAccountByTokenStub }
+}
 
 describe('Auth Middleware', () => {
   test('Should return 403 if no x-access-token exists in header ', async () => {
-    const sut = new AuthMiddleware()
+    const { sut } = makeSut()
 
     const httpResponse = await sut.handle({})
 
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Should call LoadAccountByToken with correct accessToken', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+
+    const httpRequest = {
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    }
+
+    await sut.handle(httpRequest)
+
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
 })
