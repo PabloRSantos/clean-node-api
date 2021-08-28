4655e7580d25b17b8b4c2296c25df4744966c782
Pablo <pablorsantos15@gmail.com>
2021-08-28 16:49:04 -0300

    fix: ensure AccountMongoRepository allows admin access any route

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,8 +1,11 @@
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import { env } from '../config/env'
 
+let surveyCollection: Collection
 let accountCollection: Collection
 
 describe('Survey Routes', () => {
@@ -15,7 +18,10 @@ describe('Survey Routes', () => {
   })
 
   beforeEach(async () => {
-    accountCollection = await MongoHelper.getCollection('surveys')
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -34,6 +40,36 @@ describe('Survey Routes', () => {
           }]
         })
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid token', async () => {
+      const { insertedId } = await accountCollection.insertOne({
+        name: 'Pablo',
+        email: 'pablo.rosa@mail.com',
+        password: '123',
+        role: 'admin'
+      })
+
+      const user = await accountCollection.findOne({ _id: insertedId })
+
+      const accessToken = sign({ id: user.id }, env.jwtSecret)
+
+      await accountCollection.updateOne({ _id: insertedId }, { $set: { accessToken } })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'Answer 2',
+            image: 'http://image-name.com'
+          }]
+        })
+        .expect(204)
     })
   })
 })
