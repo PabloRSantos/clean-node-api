98e10f8fd523f0293ef64a202ccdb7330a72a8b8
Pablo <pablorsantos15@gmail.com>
2021-08-28 16:30:18 -0300

    feat: ensure add survey route returns 403 if no access token is provided

---
+++ src/main/factories/usecases/account/load-account-by-token/db-load-account-by-token-factory.ts
@@ -0,0 +1,12 @@
+import { env } from 'process'
+import { DbLoadAccountByToken } from '../../../../../data/usecases/load-account-by-token/db-load-account-by-token'
+import { LoadAccountByToken } from '../../../../../domain/usecases'
+import { JwtAdapter } from '../../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../../../infra/db/mongodb/account/account-mongo-repository'
+
+export const makeDbLoadAccountByToken = (): LoadAccountByToken => {
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const accountMongoRepository = new AccountMongoRepository()
+
+  return new DbLoadAccountByToken(jwtAdapter, accountMongoRepository)
+}
