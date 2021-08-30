d11be63ad645dc324f2073f03dc9257337ef4e2b
Pablo <pablorsantos15@gmail.com>
2021-08-30 15:21:03 -0300

    test: ensure load surveys route returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -72,4 +72,51 @@ describe('Survey Routes', () => {
         .expect(204)
     })
   })
+
+  describe('GET /surveys', () => {
+    test('Should return 403 on load survey without accessToken', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
+    })
+
+    // test('Should return 200 on add survey with valid token', async () => {
+    //   const { insertedId: accountId } = await accountCollection.insertOne({
+    //     name: 'Pablo',
+    //     email: 'pablo.rosa@mail.com',
+    //     password: '123',
+    //     role: 'user'
+    //   })
+
+    //   const { insertedId: surveyId } = await surveyCollection.insertOne({
+    //     question: 'any_question',
+    //     answers: [{
+    //       image: 'any_image',
+    //       answer: 'any_answer'
+    //     }],
+    //     date: new Date()
+    //   })
+
+    //   const user = await accountCollection.findOne({ _id: accountId })
+
+    //   const accessToken = sign({ id: user.id }, env.jwtSecret)
+
+    //   await accountCollection.updateOne({ _id: accountId }, { $set: { accessToken } })
+
+    //   await request(app)
+    //     .post('/api/surveys')
+    //     .set('x-access-token', accessToken)
+    //     .send({
+    //       question: 'Question',
+    //       answers: [{
+    //         answer: 'Answer 1',
+    //         image: 'http://image-name.com'
+    //       }, {
+    //         answer: 'Answer 2',
+    //         image: 'http://image-name.com'
+    //       }]
+    //     })
+    //     .expect(204)
+    // })
+  })
 })
