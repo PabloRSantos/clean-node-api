dac34e9e5fe3c7c77019118772293a6b12956cb1
Pablo <pablorsantos15@gmail.com>
2021-08-30 14:18:12 -0300

    feat: ensure DbLoadSyrveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../domain/models'
+export * from '../../../domain/usecases'
+export * from '../../protocols/db/survey/load-surveys-repository'
+export * from '../../../domain/models'
