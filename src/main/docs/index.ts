7ceb0989e0ace1807f961acf861a358569a12532
Pablo <pablorsantos15@gmail.com>
2021-09-01 15:50:25 -0300

    docs: add add-survey api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,17 +1,36 @@
-import { badRequest, serverError, unauthorized, notFound, forbidden } from './components'
+import {
+  badRequest,
+  serverError,
+  unauthorized,
+  notFound,
+  forbidden
+} from './components'
 import { loginPath, signUpPath, surveyPath } from './paths'
-import { accountSchema, loginParamsSchema, errorSchema, surveysSchema, surveySchema, surveyAnswerSchema, apiKeyAuthSchema, signUpParamsSchema } from './schemas'
+import {
+  accountSchema,
+  loginParamsSchema,
+  errorSchema,
+  addSurveyParamsSchema,
+  surveysSchema,
+  surveySchema,
+  surveyAnswerSchema,
+  apiKeyAuthSchema,
+  signUpParamsSchema
+} from './schemas'
 
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
-    description: 'Api do curso do Mango para realizar enquetes entre programadores',
+    description:
+      'Api do curso do Mango para realizar enquetes entre programadores',
     version: '1.0.0'
   },
-  servers: [{
-    url: '/api'
-  }],
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
   tags: [
     {
       name: 'Login'
@@ -32,7 +51,8 @@ export default {
     error: errorSchema,
     surveys: surveysSchema,
     survey: surveySchema,
-    surveyAnswer: surveyAnswerSchema
+    surveyAnswer: surveyAnswerSchema,
+    addSurveyParams: addSurveyParamsSchema
   },
   components: {
     securitySchemes: {
